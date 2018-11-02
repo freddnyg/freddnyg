1d5a5f3609526d79c601dd266c4c9575a28f87bc
Freddy Nygård <freddnyg@FreddysMBP.local>
2018-11-02 08:30:16 +0100

    new job

---
+++ src/components/LeftColumn.vue
@@ -0,0 +1,79 @@
+<template>
+    <!-- Left Column -->
+    <div class="w3-third">
+    
+      <div class="w3-white w3-text-grey w3-card-4">
+        <div class="w3-display-container">
+          <img src="images/IMG_0107.jpg" style="width:100%" alt="Avatar">
+          <div class="w3-display-bottomleft w3-container w3-text-white">
+            <h2>Freddy Nygård</h2>
+          </div>
+        </div>
+        <div class="w3-bar w3-ultra-violet">
+          <router-link class="w3-bar-item w3-button" to="/">Home</router-link>
+          <router-link class="w3-bar-item w3-button" to="/about">About</router-link>
+          <router-link class="w3-bar-item w3-button" to="/webdev">Skills</router-link>
+        </div>
+        <div class="w3-container">
+          <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-ultra-violet"></i>Ingeniør</p>
+          <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-ultra-violet"></i>Bodø, Norway</p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-ultra-violet"></i><a href="mailto:mail@freddnyg.no">mail@freddnyg.no</a></p>
+          
+          <div class="w3-container skills">
+              <hr>
+          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-ultra-violet"></i>Skills</b></p>
+          <p>Autodesk Inventor</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-ultra-violet" style="width:90%">90%</div>
+          </div>
+          <p>Autodesk AutoCad</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-ultra-violet" style="width:80%">
+              <div class="w3-center w3-text-white">80%</div>
+            </div>
+          </div>
+          <router-link to="/webdev"><p>Web Design / developement</p></router-link>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-ultra-violet" style="width:75%">75%</div>
+          </div>
+          <p>Programming</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-ultra-violet" style="width:50%">50%</div>
+          </div>
+          <br>
+
+          <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-ultra-violet"></i>Languages</b></p>
+          <p>Norwegian</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-ultra-violet" style="height:24px;width:100%"></div>
+          </div>
+          <p>English</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-ultra-violet" style="height:24px;width:80%"></div>
+          </div>
+          <p>Swedish</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-ultra-violet" style="height:24px;width:55%"></div>
+          </div>
+          <br>
+        </div>
+      </div><br>
+      </div>
+
+    <!-- End Left Column -->
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'LeftColumn',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,88 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+    <LeftColumn/>
+    <!-- Right Column -->
+      <div class="w3-twothird">
+        <div id="front" class="w3-container w3-card w3-white w3-margin-bottom">
+          <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-ultra-violet"></i>Arbeidserfaring</h2>
+          <div class="w3-container">
+            <h5 class="w3-opacity"><b>Ingeniør / Industrivisualisering as</b></h5>
+            <h6 class="w3-text-ultra-violet"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jan. 2019 -></h6>
+            <p>INVIS er et ingeniørfirma som ved hjelp av høyteknologisk utstyr reduserer industriens kostnader. Vi har utviklet tids- og kostnadseffektive metoder for bruk av laserkikkert og skanner i forbindelse med konstruksjon av produksjonsutstyr til blant annet industribedrifter og olje & gass.</p>
+            <hr>
+          </div>
+          <div class="w3-container">
+            <h5 class="w3-opacity"><b>Ingeniør / Rapp Bomek as</b></h5>
+            <h6 class="w3-text-ultra-violet"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Mai 2012 - Jan. 2019</h6>
+            <p>Rapp Bomek er global leder innen design, utvikling og produksjon av avanserte sikkerhetsløsninger for dør-, vindu- og veggsystemer til energi-, sivil- og forsvarsmarkedet, verden over.</p>
+            <p>Produsere og levere dokumentasjon til kunde etter kundens spesifikasjoner. <p>Produsere og levere produksjonsunderlag for dører til eget verksted.</p> <p>Oppdatere og vedlikeholde «Mastermodeller» slik at disse til enhver tid er oppdatert i henhold
+              til siste design.</p> <p>Deltar i designutvikling.</p> <p>Har det siste året hatt en nøkkelrolle i et utviklingsprosjekt der en «rød tråd» fra marked til leveranse er temaet. Dette innebærer salgskonfigurator der dører konfigureres ut fra kundens spesifikasjoner. Dataene som blir tastet inn her blir brukt videre i hele prosessen til døren er levert(stykkliste genereres, datautvekslingsfil til 3d-modelleringsprogram genereres, skjærefile, GA-tegninger, ruter blir generert, diverse dokumentasjon som SPIR, cert. of comp. mm). Fungerer også som support for Autodesk produktene Inventor, AutoCad og Vault.</p>
+            <hr>
+          </div>
+          <div class="w3-container">
+            <h5 class="w3-opacity"><b>Ingeniør / Bomek Consulting as</b></h5>
+            <h6 class="w3-text-ultra-violet"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jul. 2007 - Apr. 2012</h6>
+            <p>Ingeniørfirma som utfører multidisipline prosjekteringsoppgaver på prosjekt for landbasert
+              industri, militære anlegg og prosjekt for petroleumsindustrien. Selskapet har sine kontorer i Bodø.</p>
+              <p>Prosjektering av pig-sender og mottaker for rengjøring av drivstoffledning Kunde: Ørlandet Hovedflystasjon.</p>
+              <p>Prosjektering av tankanlegg.
+              Kunde: Helgelandsbase as.
+              2000m3 dieseltank med tilhørende rørarrangement for mottak og leveranse av diesel over kai.</p>
+              <p>Prosjektering av diverse tankanlegg for Forsvaret. Ingeniør. Prosjektering av ny bensinledning for Statoil.
+              <p>Yara Glomfjord. Prosjektering av renseanlegg for fosfat.</p>
+              <p>Kashagan piperack.
+              Kunde: Aker Solutions Verdal.
+              Ved desentralisert engineering har vi jobbet fra Bodø mot Aker Solutions i Verdal. Vi jobbet direkte opp mot deres systemer.
+              Administrert og deltatt i produksjonen av fabrikasjonsunderlag for både hovedstål og utrustning til produksjon av piperack.
+              Programmer som ble benyttet: AutoCad (Avax og Cajak).</p>
+              <p>Valhall PH Jacket.
+              Kunde: Aker Solutions Verdal.
+              Generert fabrikasjonsunderlag for jacket fra 3D- modell, både hovedstål og utrustning. Arbeidet bl.a med gangbaner, rekkverk, ben til jacket, supporter og andre festeanordninger.
+              Utarbeidelse av fabrikasjonsunderlag for produksjon ved hjelp av AutoCad, Avax og Cajak.</p>
+              <p>Pumpe-/filterkontainer.
+              Kunde: Forsvaret.
+              Utarbeidelse av arrangement rør og komponenter ved hjelp av Inventor Pro Tube & Piping.</p>
+            <hr>
+          </div>
+          <div class="w3-container">
+            <h5 class="w3-opacity"><b>Platearbeider / Vikan Sveis as</b></h5>
+            <h6 class="w3-text-ultra-violet"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2001 - 2003</h6>
+            <p>Mekanisk verksted som driver vedlikehold og nyproduksjon av diverse utstyr innen bygg og anlegg. Driver også med utleie av personell, bandt annet til vedlikehold av produksjonsutstyr innen industrien og til verftsindustrien.</p> <p>Platearbeider/mekaniker/sveiser. Diverse rep. og nyproduksjon av blandt annet: Skuffer og kasser til hjullastere, gravere og lastebiler. Kontainere og tanker med mer.</p>
+            <p>Platearbeider/mekaniker/sveiser. Utleid til Fosen Mek. as. Har vært med på produksjon av fergene: «OLympic Champion» og «Hellenic Spirit» for det greske rederiet Anek Lines og cruiseskipet «The World» for ResidenSea.</p><br>
+          </div>
+        </div>
+
+        <div class="w3-container w3-card w3-white">
+          <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-ultra-violet"></i>Utdannelse</h2>
+          <div class="w3-container">
+            <h5 class="w3-opacity"><b>UiT i Narvik</b></h5>
+            <h6 class="w3-text-ultra-violet"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2004 - 2007</h6>
+            <p><strong>Bachelor</strong> / Industriteknikk (maskin)</p>
+            <hr>
+          </div>
+          <div class="w3-container">
+            <h5 class="w3-opacity"><b>Yara Glomfjord</b></h5>
+            <h6 class="w3-text-ultra-violet"><i class="fa fa-calendar fa-fw w3-margin-right"></i>1998 - 2000</h6>
+            <p><strong>Fagbrev</strong> / Platearbeider</p>
+            <br>
+          </div>
+        </div>
+
+      <!-- End Right Column -->
+      </div>
   </div>
+      
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import LeftColumn from '@/components/LeftColumn.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    LeftColumn
   }
 }
 </script>
